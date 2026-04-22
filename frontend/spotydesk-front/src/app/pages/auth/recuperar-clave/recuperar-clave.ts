c075a04b6c2d768b281b3f62a5236d36e34622f9
adrixzu <adrixzu@gmail.com>
2026-04-22 13:11:45 +0200

    CreacionPaginaPrincipal

--- frontend/spotydesk-front/src/app/pages/auth/recuperar-clave/recuperar-clave.ts
+++ frontend/spotydesk-front/src/app/pages/auth/recuperar-clave/recuperar-clave.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recuperar-clave',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './recuperar-clave.html',
-  styleUrl: './recuperar-clave.css',
+  styleUrl: './recuperar-clave.css'
 })
-export class RecuperarClave {}
+export class RecuperarClave {
+  // Inyectamos el enrutador
+  private router = inject(Router);
+
+  enviarEnlace() {
+    // Aquí tu compañero conectará el envío real del email en el backend
+    console.log('Simulando envío de correo de recuperación...');
+    
+    // Devolvemos al usuario a la pantalla de login
+    this.router.navigate(['/login']);
+  }
+}
--- frontend/spotydesk-front/src/app/pages/auth/registro/registro.ts
+++ frontend/spotydesk-front/src/app/pages/auth/registro/registro.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink], // Importamos RouterLink para el enlace de abajo
   templateUrl: './registro.html',
-  styleUrl: './registro.css',
+  styleUrl: './registro.css'
 })
-export class Registro {}
+export class Registro {
+  // Inyectamos el enrutador
+  private router = inject(Router);
+
+  registrarUsuario() {
+    // Aquí en el futuro llamaremos a la API de tu compañero para guardar la empresa
+    console.log('Simulando registro de nueva empresa...');
+    
+    // Y redirigimos al login
+    this.router.navigate(['/login']);
+  }
+}
