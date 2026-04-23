cb24609711d71a58623a211c470b78d48a75a3ea
adrixzu <adrixzu@gmail.com>
2026-04-23 12:52:24 +0200

    MiPerfilAñadido
    MiPerfilAñadido

--- frontend/spotydesk-front/src/app/pages/usuario/usuario.ts
+++ frontend/spotydesk-front/src/app/pages/usuario/usuario.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-usuario',
-  imports: [],
-  templateUrl: './usuario.html',
-  styleUrl: './usuario.css',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './usuario.html'
 })
-export class Usuario {}
+export class Usuario {
+  usuario = {
+    nombre: 'Alejo',
+    apellidos: 'García',
+    email: 'alejo.garcia@spotydesk.com',
+    puesto: 'Desarrollador Frontend',
+    departamento: 'Ingeniería de Software',
+    notificaciones: true,
+    
+    // --- NUEVOS DATOS EXTRAS ---
+    fechaIngreso: '2021',
+    asientoFavorito: 'Puesto 42',
+    modalidad: 'Híbrido (3 días)',
+    edificio: 'Sede Central (Planta 2)'
+  };
+
+  guardarCambios() {
+    alert('¡Cambios guardados con éxito!');
+  }
+
+  cerrarSesion() {
+    console.log('Cerrando sesión...');
+    alert('Has cerrado sesión');
+  }
+}
